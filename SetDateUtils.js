164962d4f2605199a0074f80855d514b40e794a3
AbrahamSantos <abesantostorres@gmail.com>
2024-04-12 20:58:11 -0600

    added SetDateUtils.js, form updated to  grid layout

---
+++ SetDateUtils.js
@@ -0,0 +1,13 @@
+
+// export function setCurrentDate(inputId) {
+//     const dateInput = document.getElementById(inputId);
+//     const today = new Date().toISOString().slice(0, 10); 
+//     dateInput.value = today;
+// }
+
+// // In your main app module
+// import { setCurrentDate } from './SetDateUtils.js';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     setCurrentDate('post-date');
+// });
